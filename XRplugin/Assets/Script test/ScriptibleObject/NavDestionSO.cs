a0dba895d61b222e8387f6aed9e438cf57b84083
Braxton Brown <10944268@uvu.edu>
2024-03-06 20:08:27 -0700

    Enemy moment fix / hit dectetion

--- XRplugin/Assets/Script test/ScriptibleObject/NavDestionSO.cs
+++ XRplugin/Assets/Script test/ScriptibleObject/NavDestionSO.cs
@@ -1,13 +1,19 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 [CreateAssetMenu]
 public class NavDestionSO : ScriptableObject
 {
-
+    public GameObject camera;
     public Vector3 destination;
-    public Transform navpoint;
+    public void UpdateVaule(Transform obj)
+    {
+        destination = obj.position;
+    }
+    
+    public void UpdateTransform(Transform obj)
+    {
+        obj.localPosition = destination;  
+    }
     
 }
