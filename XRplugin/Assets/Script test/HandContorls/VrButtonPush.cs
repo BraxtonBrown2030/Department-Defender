5a3df0a314fcebd3f99eca85bd2f381a04a8887b
Braxton Brown <10944268@uvu.edu>
2024-04-09 18:01:56 -0600

    Button Pushed Script Complete

--- XRplugin/Assets/Script test/HandContorls/VrButtonPush.cs
+++ XRplugin/Assets/Script test/HandContorls/VrButtonPush.cs
@@ -1,15 +1,25 @@
 using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.InputSystem;
 using UnityEngine.XR.Interaction.Toolkit;
 public class VrButtonPush : MonoBehaviour
 {
-    
+    [Tooltip("this is for the input action reference this can be found under the action map and look like bolt of lightning in a ball")]
     public InputActionReference inputAction; // This is the input action reference
+    
+    [Tooltip("This is the vr movment provider that is used to influence the xr origin rig can be found by searching for ActionBasedContinuousMoveProvider in the project search bar")]
     public ActionBasedContinuousMoveProvider moveProvider; // uses preinstalled action map to chamge default movement speed
+    
+    [Tooltip("this is the sprint speed of the movement in in movesprovider")]
+    public float maxspeed;
+    
+    [Tooltip("This is the normal speed of the movement in in movesprovider")]
+    public float minspeed;
+    
     
     void Start()
     {
-        moveProvider.moveSpeed = 1; // This is the speed of the movement in in movesprovider
+        moveProvider.moveSpeed = minspeed; // This is the speed of the movement in in movesprovider
         
     }
     
@@ -18,16 +28,13 @@ public class VrButtonPush : MonoBehaviour
 
         if (inputAction.action.ReadValue<float>() > 0.1f)
         {
-           // moveProvider.moveSpeed = 4; // This is the sprint speed of the movement in in movesprovider
-
-           Debug.Log("Button is pressed");
+           moveProvider.moveSpeed = maxspeed; // This is the sprint speed of the movement in in movesprovider
 
         }
         else
         {
-           // moveProvider.moveSpeed = 1; // This is the  walk speed of the movement in in movesprovider
            
-           Debug.Log("Button is not pressed");
+            moveProvider.moveSpeed = minspeed; // This is the  walk speed of the movement in in movesprovider
            
         }
         
