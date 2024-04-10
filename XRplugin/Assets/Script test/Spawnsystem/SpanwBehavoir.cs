ba17404a02fb5be74a2be2e8e5e28e91a4f3c01a
Braxton Brown <97472118+BraxtonBrown2030@users.noreply.github.com>
2024-04-10 17:28:49 -0600

    spawning and main screen spawn setup

--- XRplugin/Assets/Script test/Spawnsystem/SpanwBehavoir.cs
+++ XRplugin/Assets/Script test/Spawnsystem/SpanwBehavoir.cs
@@ -7,11 +7,20 @@ public class SpanwBehavoir : ScriptableObject
 {
    public Vector3DataList vector3DataListObj;
    public GameObject prefab;
+   public int num;
+ 
+      public void CreateIncstanceFromListRandomly(Vector3DataList obj)
+      {
+         num = Random.Range(0, obj.vector3List.Count - 1);
+         Instantiate(prefab, obj.vector3List[num].value, Quaternion.identity);
+      
+      }
 
-   public void Spawn()
-   {
-      
-      
-   }
-   
+      public void IncreaseSpawnAmount(int value)
+      {
+         
+         num = value;
+         
+      }
+ 
 }
