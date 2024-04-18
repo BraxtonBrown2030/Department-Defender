94134e395b0bded58984a667064027b239ddcd15
Braxton Brown <10944268@uvu.edu>
2024-04-18 11:34:50 -0600

    Help Window Setup

---
+++ XRplugin/Assets/Script test/UI Testing/CanvasTurnOff.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class CanvasTurnOff : MonoBehaviour
+{
+    public Canvas canvas;
+    public UnityEvent turnOffEvent;
+
+    public void Awake()
+    {
+        turnOffEvent.Invoke();
+    }
+
+    public IEnumerator DestroyAfterDelayCoroutine()
+    {
+        yield return new WaitForSeconds(5f);
+            canvas.enabled = false;
+    }
+    
+    public void DestroyAfterDelay()
+    {
+        StartCoroutine(DestroyAfterDelayCoroutine());
+    }
+}
